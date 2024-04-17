bc2f60d921cce0afec17e2ad630ca5efdad429e5
Edwin van de Pol <developer@edwinvandepol.nl>
2024-04-17 15:20:26 +0200

    Merge device and driver

--- drivers/tracker/driver.js
+++ drivers/tracker/driver.js
@@ -1,51 +1,8 @@
 'use strict';
 
 const Driver = require('../../lib/Driver');
-const { TrackerCapabilities } = require('../../lib/Enums');
 
 class TrackerDriver extends Driver {
-
-  /*
-  | Pairing functions
-  */
-
-  // Return devices while pairing
-  async getPairDevices({ oAuth2Client }) {
-    return oAuth2Client.discoverTrackers();
-  }
-
-  // Return capabilities while pairing
-  getPairCapabilities(device) {
-    const caps = [
-      'measure_battery',
-      'tracker_state',
-      'charging_state',
-      'battery_state',
-      'in_geofence',
-      'geofence',
-    ];
-
-    for (const [name, capabilities] of Object.entries(TrackerCapabilities)) {
-      if (device.capabilities.includes(name)) {
-        for (const capability of capabilities) {
-          caps.push(capability);
-        }
-      }
-    }
-
-    caps.push('location_source', 'altitude', 'speed', 'latitude', 'longitude');
-
-    return caps;
-  }
-
-  // Return settings value while pairing
-  getPairSettings(device) {
-    return {
-      product_name: device.product_name,
-      model_name: device.model_name,
-    };
-  }
-
 }
 
 module.exports = TrackerDriver;
